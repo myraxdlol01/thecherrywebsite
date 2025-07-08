a0559b354b94d7c3b1f71aa160266eb2c6e461d5
cherie <saintcatmusic@outlook.com>
2025-07-08 16:13:12 -0400

    eeee

--- app.py
+++ app.py
@@ -108,4 +108,4 @@ def guild_dashboard(guild_id):
     return render_template('guild_dashboard.html', guild_id=guild_id, settings=settings)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
