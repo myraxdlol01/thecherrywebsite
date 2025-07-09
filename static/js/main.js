3c0502d6740b45caf7b8c40a83708b5c1518ecd3
cherie <saintcatmusic@outlook.com>
2025-07-09 03:46:25 -0400

    Clean up debug code

--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,11 @@
 // minimal js for cherrybot dashboard
 console.log('cherrybot dashboard loaded');
+
+// basic page loading overlay
+window.addEventListener('load', () => {
+  const loader = document.getElementById('page-loader');
+  if (loader) {
+    loader.classList.add('hidden');
+    setTimeout(() => loader.remove(), 450);
+  }
+});
